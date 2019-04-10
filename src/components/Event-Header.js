b56e1cb1d8d5445705009915b54b10f8f168ad2e
Axel Baumgartner <axel.o.baumgartner@utu.fi>
2019-04-10 11:27:38 +0300

    header editor removed

--- src/components/Event-Header.js
+++ src/components/Event-Header.js
@@ -12,29 +12,6 @@ const Header = (props) => {
         <div style={headerStyles} className="Header-container">
             <h1>{props.headerData.h1}</h1>
             <h2>{props.headerData.h2}</h2>
-
-            <div className="editRow">
-                <div className="editor">
-
-                    <div className="inputBlock">
-                        <label>background url</label>
-                        <input id="urlInput"></input>
-                    </div>
-
-                    <div className="inputBlock">
-                        <label>h1 color</label>
-                        <input id="colorInput"></input>
-                    </div>
-
-                    <div className="inputBlock">
-                        <label>h2 color</label>
-                        <input id="colorInput"></input>
-                    </div>
-
-                    <button id="saveButton" onClick={props.save}></button>
-
-                </div>
-            </div>
         </div>
     )
 }
