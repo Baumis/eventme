de8178d55270984b74f8201978a10a9025f32e6b
Axel Baumgartner <axel.o.baumgartner@utu.fi>
2019-04-25 16:14:11 +0300

    component container styles

--- src/components/ComponentContainer/ComponentContainer.js
+++ src/components/ComponentContainer/ComponentContainer.js
@@ -5,11 +5,13 @@ import MasterComponent from './MasterComponent'
 const ComponentContainer = ({ components }) => {
 
     return (
-        <div className="componentContainer">
-            {components.map(component => (
-                <MasterComponent key={component.order} component={component} />
-            ))}
-        </div>
+        <div className="componentBody">
+            <div className="componentContainer">
+                {components.map(component => (
+                    <MasterComponent key={component.order} component={component} />
+                ))}
+            </div>
+        </div >
     )
 }
 export default ComponentContainer
