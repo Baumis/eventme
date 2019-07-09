3fd7639d3ddc665ee923eeb88b7ad7d8245439f5
Axel Baumgartner <axel.o.baumgartner@utu.fi>
2019-07-09 21:14:27 +0300

    mark

--- client/src/views/Profile/Profile.js
+++ client/src/views/Profile/Profile.js
@@ -19,6 +19,7 @@ class Profile extends Component {
     }
 
     render() {
+
         return (
             <div className="profileViewContainer">
                 <ProfileHeader />
--- client/src/views/Profile/components/ProfileHeader/ProfileCard.js
+++ client/src/views/Profile/components/ProfileHeader/ProfileCard.js
@@ -2,26 +2,27 @@ import React from 'react'
 import { FaUser, FaEnvelope, FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 const ProfileCard = ({ user }) => {
+
     return (
         <div className="profileCard">
             <div className="profileInfo">
                 <div>
-                    <div className="profileCircle"><FaUser /></div>
+                    <div className="profileCircle text"><FaUser /></div>
                 </div>
                 <div className="cardInfo">
-                    <div className="cardInfoRow">
+                    <div className="cardInfoRow text">
                         <h1>{user.username}</h1>
                     </div>
-                    <div className="cardInfoRow">
+                    <div className="cardInfoRow text">
                         <p>{user.name}</p>
                     </div>
-                    <div className="cardInfoRow">
+                    <div className="cardInfoRow text">
                         <FaEnvelope />
                         <p>{user.email}</p>
                     </div>
                 </div>
             </div>
-            <div className="socialRow">
+            <div className="socialRow text">
                 <FaFacebook />
                 <FaInstagram />
                 <FaTwitter />
