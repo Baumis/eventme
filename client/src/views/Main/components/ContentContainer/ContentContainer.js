4ba2fce7799827c2f98bfe73d18e46920463a0e2
Axel Baumgartner <axel.o.baumgartner@utu.fi>
2019-07-25 21:55:17 +0300

    styles for Main edited

--- client/src/views/Main/components/ContentContainer/ContentContainer.js
+++ client/src/views/Main/components/ContentContainer/ContentContainer.js
@@ -7,16 +7,14 @@ class ContentContainer extends Component {
     render() {
         return (
             <div className="content-container">
-                <div className="content-container-left-column">
-                    <div className="content-container-event-panel">
-                        <h1>{'Let\'s create your event!'}</h1>
-                        <p>{'Eventme is a great tool for creating homes for your events. Share your events and invite your friends!'}</p>
-                        <CreateButton click={this.props.click} />
-                    </div>
+                <div className="content-container-event-panel">
+                    <h1>{'Let\'s create your event!'}</h1>
+                    <p>{'Get things started by clicking the button below and use your creativity!'}</p>
+                    <CreateButton click={this.props.click} />
                 </div>
-                <div className="content-container-rigth-column">
-                    <div className="content-container-decoration">
-                    </div>
+                <div className="content-container-event-panel">
+                    <h1>{'What is EvenMe?'}</h1>
+                    <p>{'Eventme is a great tool for creating homes for your events. Share your events and invite your friends!'}</p>
                 </div>
             </div>
         )
