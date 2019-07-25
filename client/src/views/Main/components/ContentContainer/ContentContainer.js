43148c2a5cecbd7d956c47985e1ea8814fa75024
Axel Baumgartner <axel.o.baumgartner@utu.fi>
2019-07-25 20:57:03 +0300

    panel behind createButton

--- client/src/views/Main/components/ContentContainer/ContentContainer.js
+++ client/src/views/Main/components/ContentContainer/ContentContainer.js
@@ -8,9 +8,11 @@ class ContentContainer extends Component {
         return (
             <div className="content-container">
                 <div className="content-container-left-column">
-                    <h1>{'Let\'s create your event!'}</h1>
-                    <p>{'Eventme is a great tool for creating homes for your events. Share your events and invite your friends!'}</p>
-                    <CreateButton click={this.props.click} />
+                    <div className="content-container-event-panel">
+                        <h1>{'Let\'s create your event!'}</h1>
+                        <p>{'Eventme is a great tool for creating homes for your events. Share your events and invite your friends!'}</p>
+                        <CreateButton click={this.props.click} />
+                    </div>
                 </div>
                 <div className="content-container-rigth-column">
                     <div className="content-container-decoration">
