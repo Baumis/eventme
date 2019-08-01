7b4da1b2a6fc913222376da4b72f6169c6d5d18b
jesushilden <peter.e.hilden@gmail.com>
2019-08-01 15:36:59 +0300

    finished userController refactoring

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
+const Event = require('../models/event')
 
 exports.getAllPopulated = async () => {
     return await User
@@ -48,4 +49,63 @@ exports.create = async (userObject) => {
     }
 
     return await user.save()
+}
+
+exports.update = async (id, userObject) => {
+    const user = await User.findById(id)
+
+    if (!user) {
+        throw new Error('Malformatted id')
+    }
+
+    user.username = userObject.username || user.username
+    user.name = userObject.name || user.name
+    user.email = userObject.email || user.email
+
+    const error = user.validateSync()
+
+    if (error) {
+        const errorMessages = Object.keys(error.errors).map(field => error.errors[field])
+        throw new Error(errorMessages.join(', '))
+    }
+
+    const savedUser = await user.save()
+
+    return await savedUser
+        .populate('myEvents', { _id: 1, label: 1, background: 1 })
+        .populate('myInvites', { _id: 1, label: 1, background: 1 })
+        .execPopulate()
+}
+
+exports.delete = async (id) => {
+    const user = await User.findById(id)
+
+    if (!user) {
+        throw new Error('Malformatted id')
+    }
+
+    const myEventsPromises = user.myEvents.map(eventId => Event.findById(eventId))
+    const myEvents = await Promise.all(myEventsPromises)
+
+    for (let myEvent of myEvents) {
+        const myEventGuestsPromises = myEvent.guests.map(guest => User.findById(guest.user))
+        const myEventGuests = await Promise.all(myEventGuestsPromises)
+
+        for (let myEventGuest of myEventGuests) {
+            myEventGuest.myInvites = myEventGuest.myInvites.filter(eventId => eventId !== myEvent._id)
+            await myEventGuest.save()
+        }
+    }
+
+    await Event.deleteMany({creator: user._id})
+
+    const myInvitesPromises = user.myInvites.map(eventId => Event.findById(eventId))
+    const myInvites = await Promise.all(myInvitesPromises)
+
+    for (let myInvite of myInvites) {
+        myInvite.guests = myInvite.guests.filter(guest => guest.user.toString() !== user._id.toString())
+        await myInvite.save()
+    }
+
+    await user.remove()
 }
