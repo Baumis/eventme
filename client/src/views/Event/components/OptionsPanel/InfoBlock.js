b88b18754b0835d8faabca1a6b143be7d2db597d
baumis <baumgartneraxel@gmail.com>
2019-08-06 17:03:12 +0300

    style fixes

--- client/src/views/Event/components/OptionsPanel/InfoBlock.js
+++ client/src/views/Event/components/OptionsPanel/InfoBlock.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './Options.css'
-import { FaTimes, FaPhone, FaAt, FaInfo, FaCalendar, FaLocationArrow, FaClock, FaUser, FaEmptySet } from 'react-icons/fa'
+import { FaTimes, FaPhone, FaAt, FaInfo, FaCalendar, FaLocationArrow, FaClock, FaUser } from 'react-icons/fa'
 
 class InfoBlock extends Component {
 
@@ -39,7 +39,7 @@ class InfoBlock extends Component {
                 {this.state.displayMenu ?
                     <div className="options-panel-icon-menu">
                         <div className="options-panel-icon-menu-item" onClick={() => this.props.changeIcon('EMAIL', this.props.index)}><FaAt /></div>
-                        <div className="options-panel-icon-menu-item" onClick={() => this.props.changeIcon('CALENDAR', this.props.index)}><FaCalendar /></div>
+                        <div className="options-panel-icon-menu-item" onClick={() => this.props.changeIcon('DATE', this.props.index)}><FaCalendar /></div>
                         <div className="options-panel-icon-menu-item" onClick={() => this.props.changeIcon('CONTACT', this.props.index)}><FaUser /></div>
                         <div className="options-panel-icon-menu-item" onClick={() => this.props.changeIcon('INFO', this.props.index)}><FaInfo /></div>
                         <div className="options-panel-icon-menu-item" onClick={() => this.props.changeIcon('LOCATION', this.props.index)}><FaLocationArrow /></div>
