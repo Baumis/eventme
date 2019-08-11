68d9381af5128ad130052b31862b510799aceec8
jesushilden <peter.e.hilden@gmail.com>
2019-08-11 18:50:36 +0300

    transactions implemented

--- server/services/eventService.js
+++ server/services/eventService.js
@@ -18,45 +18,58 @@ exports.populate = async (event) => {
 }
 
 exports.create = async (creatorId, eventObject) => {
-    const user = await User.findById(creatorId)
+    const session = await Event.startSession()
+    session.startTransaction()
+    const options = { session }
 
-    const startDate = eventObject.startDate ? new Date(eventObject.startDate) : new Date()
-    const endDate = eventObject.endDate ? new Date(eventObject.endDate) : new Date(startDate.getDate() + 1)
+    try {
+        const user = await User.findById(creatorId)
 
-    if (startDate > endDate) {
-        throw new Error('End Date must be greater than Start Date')
+        const startDate = eventObject.startDate ? new Date(eventObject.startDate) : new Date()
+        const endDate = eventObject.endDate ? new Date(eventObject.endDate) : new Date(startDate.getTime() + 1000 * 60 * 60 * 24)
+
+        if (startDate > endDate) {
+            throw new Error('End Date must be greater than Start Date')
+        }
+
+        const newEvent = new Event({
+            label: eventObject.label,
+            startDate: startDate,
+            endDate: endDate,
+            creator: user._id,
+            background: eventObject.background,
+            infoPanel: eventObject.infoPanel,
+            guests: [{
+                user: user._id,
+                status: 'GOING'
+            }],
+            components: eventObject.components
+        })
+
+        const error = newEvent.validateSync()
+
+        if (error) {
+            const errorMessages = Object.keys(error.errors).map(field => error.errors[field])
+            throw new Error(errorMessages.join(', '))
+        }
+
+        user.myEvents = user.myEvents.concat(newEvent._id)
+
+        await user.save(options)
+        const savedEvent = await newEvent.save(options)
+
+        const populatedEvent = await this.populate(savedEvent)
+
+        await session.commitTransaction()
+        session.endSession()
+
+        return populatedEvent
+
+    } catch (exception) {
+        await session.abortTransaction()
+        session.endSession()
+        throw new Error('Could not create event')
     }
-
-    const newEvent = new Event({
-        label: eventObject.label,
-        startDate: startDate,
-        endDate: endDate,
-        creator: user._id,
-        background: eventObject.background,
-        infoPanel: eventObject.infoPanel,
-        guests: [{
-            user: user._id,
-            status: 'GOING'
-        }],
-        components: eventObject.components
-    })
-
-    const error = newEvent.validateSync()
-
-    if (error) {
-        const errorMessages = Object.keys(error.errors).map(field => error.errors[field])
-        throw new Error(errorMessages.join(', '))
-    }
-
-    user.myEvents = user.myEvents.concat(newEvent._id)
-
-    const savedEvent = await newEvent.save()
-    await user.save()
-
-    return await savedEvent
-        .populate('creator', { _id: 1, name: 1 })
-        .populate('guests.user', { _id: 1, name: 1 })
-        .execPopulate()
 }
 
 exports.update = async (event, eventObject) => {
@@ -91,61 +104,101 @@ exports.update = async (event, eventObject) => {
 }
 
 exports.delete = async (event) => {
-    const creator = await User.findById(event.creator._id)
+    const session = await Event.startSession()
+    session.startTransaction()
+    const options = { session }
 
-    creator.myEvents = creator.myEvents.filter(eventId => eventId !== event._id)
+    try {
+        const creator = await User.findById(event.creator._id)
 
-    await creator.save()
+        creator.myEvents = creator.myEvents.filter(eventId => eventId !== event._id)
 
-    const guestsPromises = event.guests.map(guest => User.findById(guest.user))
-    const guests = await Promise.all(guestsPromises)
+        await creator.save(options)
 
-    for (guest of guests) {
-        guest.myInvites = guest.myInvites.filter(eventId => eventId !== event._id)
-        await guest.save()
+        const guestsPromises = event.guests.map(guest => User.findById(guest.user))
+        const guests = await Promise.all(guestsPromises)
+
+        for (guest of guests) {
+            guest.myInvites = guest.myInvites.filter(eventId => eventId !== event._id)
+            await guest.save(options)
+        }
+
+        await event.remove(options)
+
+        await session.commitTransaction()
+        session.endSession()
+
+    } catch (exception) {
+        await session.abortTransaction()
+        session.endSession()
+        throw new Error('Could not remove event')
     }
-
-    await event.remove()
 }
 
 exports.addGuest = async (event, guestId) => {
-    const guest = event.guests.find(guest => guest.user.toString() === guestId)
+    const session = await Event.startSession()
+    session.startTransaction()
+    const options = { session }
 
-    if (guest) {
-        throw new Error('User is already a guest')
+    try {
+        const guest = event.guests.find(guest => guest.user.toString() === guestId)
+
+        if (guest) {
+            throw new Error('User is already a guest')
+        }
+
+        const user = await User.findById(guestId)
+
+        event.guests = event.guests.concat({
+            user: user._id,
+            status: 'PENDING'
+        })
+
+        user.myInvites = user.myInvites.concat(event._id)
+
+        const savedEvent = await event.save(options)
+        await user.save(options)
+
+        const populatedEvent = await this.populate(savedEvent)
+
+        await session.commitTransaction()
+        session.endSession()
+
+        return populatedEvent
+
+    } catch (exception) {
+        await session.abortTransaction()
+        session.endSession()
+        throw new Error('Could not add guest')
     }
-
-    const user = await User.findById(guestId)
-
-    event.guests = event.guests.concat({
-        user: user._id,
-        status: 'PENDING'
-    })
-
-    user.myInvites = user.myInvites.concat(event._id)
-
-    const savedEvent = await event.save()
-    await user.save()
-
-    return await savedEvent
-        .populate('creator', { _id: 1, name: 1 })
-        .populate('guests.user', { _id: 1, name: 1 })
-        .execPopulate()
 }
 
 exports.removeGuest = async (event, guestId) => {
-    const user = await User.findById(guestId)
+    const session = await Event.startSession()
+    session.startTransaction()
+    const options = { session }
 
-    event.guests = event.guests.filter(guest => guest.user.toString() !== guestId)
-    user.myInvites = user.myInvites.filter(event => event !== user._id)
+    try {
+        const user = await User.findById(guestId)
 
-    const savedEvent = await event.save()
-    await user.save()
+        event.guests = event.guests.filter(guest => guest.user.toString() !== guestId)
+        user.myInvites = user.myInvites.filter(event => event !== user._id)
 
-    return await savedEvent
-        .populate('creator', { _id: 1, name: 1 })
-        .populate('guests.user', { _id: 1, name: 1 })
-        .execPopulate()
+        const savedEvent = await event.save(options)
+        await user.save(options)
+
+        const populatedEvent = await this.populate(savedEvent)
+
+        await session.commitTransaction()
+        session.endSession()
+
+        return populatedEvent
+
+    } catch (exception) {
+        await session.abortTransaction()
+        session.endSession()
+        throw new Error('Could not remove guest')
+    }
 }
 
 exports.changeInviteKey = async (event) => {
