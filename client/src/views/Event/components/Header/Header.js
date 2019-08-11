2e64d053799fbe1a5cf94230e168eeb033288661
baumis <baumgartneraxel@gmail.com>
2019-08-11 13:30:28 +0300

    added moment and formated date in header

--- client/src/views/Event/components/Header/Header.js
+++ client/src/views/Event/components/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import InfoPanel from './InfoPanel.js'
 import StatusBar from '../StatusBar/StatusBar'
+import moment from 'moment'
 import './Header.css'
 
 class Header extends Component {
@@ -15,7 +16,7 @@ class Header extends Component {
             <div style={headerStyles} className="event-header">
                 <div className="event-header-content">
                     <h1>{this.props.EventStore.event.label}</h1>
-                    <h3>{this.props.EventStore.event.startDate}</h3>
+                    <h3>{moment(this.props.EventStore.event.startDate).format("MMM Do YY")}</h3>
                 </div>
                 {this.props.isGuest ?
                     <div className="event-header-status-row">
