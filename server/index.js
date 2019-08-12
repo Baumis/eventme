884200fbd3fcdef4d7bc5fc80e55efcf47760a11
jesushilden <peter.e.hilden@gmail.com>
2019-08-12 21:20:10 +0300

    removed unused import

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const http = require('http')
 const express = require('express')
-const path = require('path')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
