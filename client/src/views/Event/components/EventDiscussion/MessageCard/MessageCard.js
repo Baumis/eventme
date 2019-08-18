1f531efbf128d7be3c87012e9f4fbcc7d02ef730
baumis <baumgartneraxel@gmail.com>
2019-08-18 19:57:18 +0300

    master merged and discussion component displays messages

--- client/src/views/Event/components/EventDiscussion/MessageCard/MessageCard.js
+++ client/src/views/Event/components/EventDiscussion/MessageCard/MessageCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import './MessageCard.css'
 import { FaTrash } from 'react-icons/fa'
+import Moment from 'moment'
 
-const MessageCard = ({ props }) => {
+const MessageCard = ( props ) => {
     return (
         <div className="message-card">
             <div className="message-card-header">
                 <div className="message-card-time">
-                    {props.message.time}
+                    {Moment(props.message.time).format('h:mm')}
                 </div>
                 <div className="message-card-author">
                     {props.message.author.name}
