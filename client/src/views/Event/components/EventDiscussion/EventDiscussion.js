4640fa1fbd15038a962fcefacd3647b1f958643f
baumis <baumgartneraxel@gmail.com>
2019-08-18 20:41:25 +0300

    messages can be posted

--- client/src/views/Event/components/EventDiscussion/EventDiscussion.js
+++ client/src/views/Event/components/EventDiscussion/EventDiscussion.js
@@ -5,6 +5,21 @@ import MessageCard from './MessageCard/MessageCard'
 
 class EventDiscussion extends Component {
 
+    constructor(props){
+        super(props)
+        this.state={
+            messageInput: ''
+        }
+    }
+
+    post = () => {
+        this.props.EventStore.postMessage(this.state.messageInput)
+    }
+
+    changeInputValue = (event) => {
+        this.setState({[event.target.name]: event.target.value})
+    }
+
     isAuthor = (id) => {
         return this.props.isCreator || this.props.UserStore.currentUser._id === id
     }
@@ -17,8 +32,13 @@ class EventDiscussion extends Component {
                         <h3>Discussion</h3>
                     </div>
                     <div className="discussion-input-row">
-                        <input></input>
-                        <div className="discussion-send-button">
+                        <input
+                            name="messageInput"
+                            onChange={this.changeInputValue}
+                            value={this.state.messageInput}
+                            placeholder={'your message'}
+                        />
+                        <div className="discussion-send-button" onClick={() => this.post()}>
                             Post
                         </div>
                     </div>
