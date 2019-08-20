fdcdc61e295f98b073fa090e9ee270a6153e1f63
baumis <baumgartneraxel@gmail.com>
2019-08-20 18:04:42 +0300

    messages can be deleted

--- client/src/views/Event/components/EventDiscussion/MessageCard/MessageCard.js
+++ client/src/views/Event/components/EventDiscussion/MessageCard/MessageCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './MessageCard.css'
 import { withRouter } from 'react-router-dom'
-import { FaTrash } from 'react-icons/fa'
+import { FaTimes } from 'react-icons/fa'
 import Moment from 'moment'
 
 const MessageCard = (props) => {
@@ -16,27 +16,34 @@ const MessageCard = (props) => {
 
     return (
         <div className="message-card">
-            <div className="message-card-avatar-section">
-                <div style={getAvatar()} className="message-card-avatar"> </div>
-            </div>
-            <div className="message-card-info-section">
-                <div className="message-card-header">
-                    <div className="message-card-author" onClick={() => toProfile(props.message.author._id)}>
-                        {props.message.author.name}
+            {!props.message.author ?
+                <div className="message-card-wrapper" id="deleted">
+                    deleted
+                </div>
+                :
+                <div className="message-card-wrapper">
+                    <div className="message-card-avatar-section">
+                        <div style={getAvatar()} className="message-card-avatar"> </div>
                     </div>
-                    <div className="message-card-time">
-                        {Moment(props.message.time).format(' D MM YYYY hh:mm')}
-                    </div>
-                    {props.isOwner ?
-                        <div className="message-card-delete">
-                            <FaTrash />
+                    <div className="message-card-info-section">
+                        <div className="message-card-header">
+                            <div className="message-card-author" onClick={() => toProfile(props.message.author._id)}>
+                                {props.message.author.name}
+                            </div>
+                            <div className="message-card-time">
+                                {Moment(props.message.time).format(' D MM YYYY hh:mm')}
+                            </div>
+                            {props.isAuthor(props.message.author._id) ?
+                                <div className="message-card-delete" onClick={() => props.delete(props.message._id)}>
+                                    <FaTimes />
+                                </div>
+                                : null}
                         </div>
-                        : null}
-                </div>
-                <div className="message-card-content">
-                    {props.message.content}
-                </div>
-            </div>
+                        <div className="message-card-content">
+                            {props.message.content}
+                        </div>
+                    </div>
+                </div>}
         </div>
     )
 }
