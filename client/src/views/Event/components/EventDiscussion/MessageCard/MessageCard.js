f192fd2478640ffb3f71622b16f597d24a823dbe
baumis <baumgartneraxel@gmail.com>
2019-08-20 17:09:24 +0300

    message author name as link

--- client/src/views/Event/components/EventDiscussion/MessageCard/MessageCard.js
+++ client/src/views/Event/components/EventDiscussion/MessageCard/MessageCard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './MessageCard.css'
+import { withRouter } from 'react-router-dom'
 import { FaTrash } from 'react-icons/fa'
 import Moment from 'moment'
 
@@ -9,6 +10,10 @@ const MessageCard = (props) => {
         return { backgroundImage: `url(${props.message.author.avatar})` }
     }
 
+    const toProfile = (id) => {
+        props.history.push(`/profile/${id}`)
+    }
+
     return (
         <div className="message-card">
             <div className="message-card-avatar-section">
@@ -16,7 +21,7 @@ const MessageCard = (props) => {
             </div>
             <div className="message-card-info-section">
                 <div className="message-card-header">
-                    <div className="message-card-author">
+                    <div className="message-card-author" onClick={() => toProfile(props.message.author._id)}>
                         {props.message.author.name}
                     </div>
                     <div className="message-card-time">
@@ -36,4 +41,4 @@ const MessageCard = (props) => {
     )
 }
 
-export default MessageCard
+export default withRouter(MessageCard)
