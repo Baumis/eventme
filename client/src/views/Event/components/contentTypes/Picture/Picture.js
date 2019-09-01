6aec223d1f881629f4a34a05c2e4bc2e9d8ee261
baumis <baumgartneraxel@gmail.com>
2019-09-01 11:18:19 +0300

    picture url can be changed

--- client/src/views/Event/components/contentTypes/Picture/Picture.js
+++ client/src/views/Event/components/contentTypes/Picture/Picture.js
@@ -7,8 +7,23 @@ const Picture = (props) => {
         return { backgroundImage: `url(${props.data.url})` }
     }
 
+    const changeUrl = (event) => {
+        const dataObject = {
+            url: event.target.value
+        }
+        props.changeData(dataObject)
+    }
+
     return (
         <div style={getPicture()} className="picture-container">
+            {props.edit ?
+                <div className="picture-url-row">
+                    <input
+                        value={props.data.url}
+                        onChange={changeUrl}
+                    />
+                </div>
+                : null}
         </div>
     )
 }
