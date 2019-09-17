93bee58476db0a58215b84298d6d814b62a64529
jesushilden <peter.e.hilden@gmail.com>
2019-09-17 20:21:09 +0300

    refactored classNames of tabs

--- client/src/views/Event/components/Tabs/Tabs.js
+++ client/src/views/Event/components/Tabs/Tabs.js
@@ -5,21 +5,22 @@ class Tabs extends Component {
 
     render() {
 
+        const getActivityClass = (tabName) => {
+            return this.props.active === tabName ? ' active-tab' : ' normal-cathegory'
+        }
+
         return (
             <div className="tabs-row">
                 <div className="tabs-container">
-                    <div className="tabs-tab"
-                        id={this.props.active === 'Event' ?  'active-tab' : 'normal-cathegory'}
+                    <div className={'tabs-tab' + getActivityClass('Event')}
                         onClick={() => this.props.changeActive('Event')}>
                         Event
                     </div>
-                    <div className="tabs-tab"
-                        id={this.props.active === 'Discussion' ? 'active-tab' : 'normal-cathegory'}
+                    <div className={'tabs-tab' + getActivityClass('Discussion')}
                         onClick={() => this.props.changeActive('Discussion')}>
                         Discussion
                     </div>
-                    <div className="tabs-tab"
-                        id={this.props.active === 'Guests' ? 'active-tab' : 'normal-cathegory'}
+                    <div className={'tabs-tab' + getActivityClass('Guests')}
                         onClick={() => this.props.changeActive('Guests')}>
                         Guests
                     </div>
