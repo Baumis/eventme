32cf3611b01679bf50c5079252f6e25e94bac989
Baumis <baumgartneraxel@gmail.com>
2019-10-13 19:51:13 +0300

    scroll effect for header

--- client/src/views/Event/components/EventContent/EventContent.js
+++ client/src/views/Event/components/EventContent/EventContent.js
@@ -3,10 +3,15 @@ import './EventContent.css'
 import ComponentContainer from '../ComponentContainer/ComponentContainer'
 import DiscussionContainer from '../DiscussionContainer/DiscussionContainer'
 import GuestContainer from '../GuestContainer/GuestContainer'
+import Tabs from '../Tabs/Tabs'
 
 const EventContent = (props) => {
     return (
         <div className="event-content">
+            <Tabs 
+                active={props.activeTab}
+                changeActive={props.changeActive}
+            />
             {props.activeTab === "Event" ?
                 <ComponentContainer
                     isCreator={props.isCreator}
