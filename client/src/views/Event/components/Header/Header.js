37815186f676c4ae7e1e2bc1006cc6e9b4d9d375
baumis <baumgartneraxel@gmail.com>
2019-10-22 13:31:09 +0300

    removed scroll listener on unmount

--- client/src/views/Event/components/Header/Header.js
+++ client/src/views/Event/components/Header/Header.js
@@ -6,37 +6,39 @@ import './Header.css'
 
 class Header extends Component {
 
-componentDidMount(){
-    window.addEventListener('scroll', () => {
-        this.scrollEffect()
-    })
-}
-
-scrollEffect = () => {
-    const element = document.getElementById('slower-picture')
-    const yPosition = window.pageYOffset / 20
-    element.style.top = yPosition + '%'
-}
-
-render() {
-    const headerStyles = {
-        color: 'white',
-        backgroundImage: 'linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)),' + ' url(' + this.props.EventStore.event.background + ')'
+    componentDidMount() {
+        window.addEventListener('scroll', this.scrollEffect)
     }
-    return (
-        <div style={headerStyles} className="event-header" id="slower-picture">
-            <div className="event-header-content">
-                <h1>{this.props.EventStore.event.label}</h1>
-                <h3>{moment(this.props.EventStore.event.startDate).format('DD.MM.YYYY')}</h3>
-            </div>
-            {this.props.isGuest ?
-                <div className="event-header-status-row">
-                    <StatusBar />
-                </div>
-                : null}
-        </div>
-    )
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.scrollEffect)
 }
+
+    scrollEffect = () => {
+        const element = document.getElementById('slower-picture')
+        const yPosition = window.pageYOffset / 20
+        element.style.top = yPosition + '%'
+    }
+
+    render() {
+        const headerStyles = {
+            color: 'white',
+            backgroundImage: 'linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)),' + ' url(' + this.props.EventStore.event.background + ')'
+        }
+        return (
+            <div style={headerStyles} className="event-header" id="slower-picture">
+                <div className="event-header-content">
+                    <h1>{this.props.EventStore.event.label}</h1>
+                    <h3>{moment(this.props.EventStore.event.startDate).format('DD.MM.YYYY')}</h3>
+                </div>
+                {this.props.isGuest ?
+                    <div className="event-header-status-row">
+                        <StatusBar />
+                    </div>
+                    : null}
+            </div>
+        )
+    }
 }
 
 export default inject('EventStore')(observer(Header))
