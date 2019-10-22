5fe4ccf6bd06a3e1e1576a4813e54e543eccf219
Baumis <baumgartneraxel@gmail.com>
2019-10-22 21:40:42 +0300

    removed filter from header

--- client/src/views/Event/components/Header/Header.js
+++ client/src/views/Event/components/Header/Header.js
@@ -23,7 +23,7 @@ class Header extends Component {
     render() {
         const headerStyles = {
             color: 'white',
-            backgroundImage: 'linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)),' + ' url(' + this.props.EventStore.event.background + ')'
+            backgroundImage: 'url(' + this.props.EventStore.event.background + ')'
         }
         return (
             <div style={headerStyles} className="event-header" id="slower-picture">
