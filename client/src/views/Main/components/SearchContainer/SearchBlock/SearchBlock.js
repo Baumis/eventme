022c61e401007c71cd0063ef01a8b69410e844d8
Baumis <baumgartneraxel@gmail.com>
2019-10-31 19:22:52 +0200

    mobile size

--- client/src/views/Main/components/SearchContainer/SearchBlock/SearchBlock.js
+++ client/src/views/Main/components/SearchContainer/SearchBlock/SearchBlock.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './SearchBlock.css'
+import { FaSearch } from 'react-icons/fa'
 
 const SearchBlock = () => {
-    return(
+    return (
         <div className="search-block">
             <div className="search-block-title">
                 Search events and people.
             </div>
-            <input/>
+            <div className="search-block-input">
+                <input />
+                <div className="search-block-button">
+                    <FaSearch />
+                </div>
+            </div>
         </div>
     )
 }
