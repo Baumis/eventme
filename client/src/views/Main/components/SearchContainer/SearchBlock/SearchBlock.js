d93f512a5c6ecc421b58bf1b535bcbe356d69cd7
baumis <baumgartneraxel@gmail.com>
2019-10-31 10:41:29 +0200

    searchbar on header

---
+++ client/src/views/Main/components/SearchContainer/SearchBlock/SearchBlock.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './SearchBlock.css'
+
+const SearchBlock = () => {
+    return(
+        <div className="search-block">
+            <div className="search-block-title">
+                Search events and people.
+            </div>
+            <input/>
+        </div>
+    )
+}
+
+export default SearchBlock
--- client/src/views/Main/components/SearchContainer/SearchContainer.js
+++ client/src/views/Main/components/SearchContainer/SearchContainer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './SearchContainer.css'
+import SearchBlock from './SearchBlock/SearchBlock'
 
 const SearchContainer = () => {
     return(
         <div className="search-container">
-
+            <SearchBlock />
         </div>
     )
 }
