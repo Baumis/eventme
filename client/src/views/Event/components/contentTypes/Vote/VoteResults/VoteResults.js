7943ba27a5bbdfce3a71ab2d0f7f7be626b67ac8
Baumis <baumgartneraxel@gmail.com>
2019-12-11 21:44:40 +0200

    modal for voters

--- client/src/views/Event/components/contentTypes/Vote/VoteResults/VoteResults.js
+++ client/src/views/Event/components/contentTypes/Vote/VoteResults/VoteResults.js
@@ -1,44 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './VoteResults.css'
+import UniversalModal from '../../../../../../commonComponents/UniversalModal/UniversalModal'
+import VoteUsers from '../VoteUsers/VoteUsers'
 
-const VoteResults = (props) => {
+class VoteResults extends Component {
 
-    const votePercentage = (id) => {
+    constructor(props) {
+        super(props)
+        this.state = {
+            showVoters: false,
+            displayedOption: this.props.options[0]
+        }
+    }
+
+    showVoters = (option) => {
+        this.setState({
+            showVoters: true,
+            displayedOption: option
+        })
+    }
+
+    hideVoters = () => {
+        this.setState({ showVoters: false })
+    }
+
+    votePercentage = (id) => {
         let allVotes = 0
-        for (let option of props.options) {
+        for (let option of this.props.options) {
             allVotes += option.votes.length
         }
-        const targetVoteCount = voteCount(id)
+        const targetVoteCount = this.voteCount(id)
         return (targetVoteCount / allVotes) * 100
     }
 
-    const voteCount = (optionId) => {
-        const option = props.options.find(option => option._id === optionId)
+    voteCount = (optionId) => {
+        const option = this.props.options.find(option => option._id === optionId)
         return option.votes.length
     }
 
-    return (
-        <div className="vote-component-results">
-            {props.options.map((option, i) =>
-                <div key={i} className="vote-component-option-container">
-                    <div className="vote-component-title-row">
-                        <div className={"vote-component-title"}>
-                            <div>{option.label}</div>
-                            <div className="vote-component-vote-count">{option.votes.length + ' votes'}</div>
+    render() {
+        return (
+            <div className="vote-component-results">
+                {this.state.showVoters ?
+                    <UniversalModal
+                        content={<VoteUsers
+                            option={this.state.displayedOption}
+                            close={this.hideVoters}
+                        />}
+                    />
+                    : null}
+                {this.props.options.map((option, i) =>
+                    <div key={i} className="vote-component-option-container">
+                        <div className="vote-component-title-row">
+                            <div className={"vote-component-title"}>
+                                <div>{option.label}</div>
+                                <div className="vote-component-vote-count" onClick={() => this.showVoters(option)}>
+                                    {option.votes.length + ' votes'}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="vote-component-status-bar">
+                            <div style={{ width: this.votePercentage(option._id) + '%' }} className="vote-component-status-filler" />
                         </div>
                     </div>
-                    <div className="vote-component-status-bar">
-                        <div style={{ width: votePercentage(option._id) + '%' }} className="vote-component-status-filler" />
+                )}
+                <div className="vote-component-button-row">
+                    <div className="vote-component-back-button" onClick={() => this.props.toggleResults(false)}>
+                        {'Vote'}
                     </div>
                 </div>
-            )}
-            <div className="vote-component-button-row">
-                <div className="vote-component-back-button" onClick={() => props.toggleResults(false)}>
-                    {'Vote'}
-                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default VoteResults 
