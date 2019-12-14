4bbba0c5a30ea24a91593168b54619af2ddbc446
Baumis <baumgartneraxel@gmail.com>
2019-12-14 16:31:35 +0200

    cleaned up text component

--- client/src/views/Event/components/contentTypes/Text/Text.js
+++ client/src/views/Event/components/contentTypes/Text/Text.js
@@ -1,24 +1,7 @@
 import React from 'react'
 import './Text.css'
-import EditableWrapper from '../../EditableWrapper/EditableWrapper'
 
 const Text = (props) => {
-
-    const changeTitle = (event) => {
-        const dataObject = {
-            title: event.target.value,
-            content: props.component.data.content
-        }
-        props.changeData(dataObject)
-    }
-
-    const changeContent = (event) => {
-        const dataObject = {
-            title: props.component.data.title,
-            content: event.target.value
-        }
-        props.changeData(dataObject)
-    }
     return (
         <div className="text-component">
             <div className={"text-component-title"}>
