2638a23c482d1427abbb6dc8913e3771bb0c5b6b
Baumis <baumgartneraxel@gmail.com>
2020-01-23 16:11:09 +0200

    displaying answers in guests

--- client/src/views/Event/components/GuestContainer/RegisterResults/AnswerSection/AnswerSection.js
+++ client/src/views/Event/components/GuestContainer/RegisterResults/AnswerSection/AnswerSection.js
@@ -3,28 +3,41 @@ import './AnswerSection.css'
 import { FaChevronDown } from 'react-icons/fa'
 
 class AnswerSection extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            display: false
-        }
-    }
 
-    toggleDisplay = () => {
-        this.setState({ display: !this.state.display })
+    getAvatar = (user) => {
+        if (!user.avatar) {
+            return null
+        }
+        return { backgroundImage: `url(${user.avatar})` }
     }
 
     render() {
 
-        if (!this.state.display) {
+        if (!this.props.show) {
             return null
+        }
+
+        if (this.props.question.answers.length < 1) {
+            return (
+                <div className="answer empty-answers">
+                    No answers to display.
+                </div>)
         }
 
         return (
             <div className="answer-section">
                 {this.props.question.answers.map((answer, i) =>
                     <div className="answer">
-                        {answer.content}
+                        <div className="answers-user-info">
+                            <div className="answers-user-avatar" style={this.getAvatar(answer.user)}>
+                            </div>
+                            <div className="answers-user-name">
+                                {answer.user.name}
+                            </div>
+                        </div>
+                        <div className="answers-user-content">
+                            {answer.content}
+                        </div>
                     </div>
                 )}
             </div>
--- client/src/views/Event/components/GuestContainer/RegisterResults/RegisterResults.js
+++ client/src/views/Event/components/GuestContainer/RegisterResults/RegisterResults.js
@@ -2,22 +2,42 @@ import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
 import { withRouter } from 'react-router-dom'
 import './RegisterResults.css'
-import { FaChevronDown } from 'react-icons/fa'
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa'
 import AnswerSection from './AnswerSection/AnswerSection'
 
 class RegisterResults extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            displayedAnswers: []
+        }
+    }
+
+    toggleAnswer = (id) => {
+        if (this.state.displayedAnswers.includes(id)) {
+            this.setState({ displayedAnswers: this.state.displayedAnswers.filter(answerId => answerId !== id) })
+        } else {
+            this.setState({ displayedAnswers: [... this.state.displayedAnswers, id] })
+        }
+    }
 
     render() {
         return (
             <div className="register-results">
                 {this.props.EventStore.questions.map((question, i) =>
                     <div className="register-results-question" key={i}>
-                        <div className="register-results-question-button">
+                        <div className="register-results-question-button" onClick={() => this.toggleAnswer(i)}>
                             {question.content}
-                            <FaChevronDown />
+                            {this.state.displayedAnswers.includes(i) ?
+                                <FaChevronUp />
+                                :
+                                <FaChevronDown />
+                            }
                         </div>
                         <AnswerSection
                             question={question}
+                            show={this.state.displayedAnswers.includes(i)}
                         />
                     </div>
                 )}
