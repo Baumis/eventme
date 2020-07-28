c94fb8e3308653289aa21ae6a3080b315c065562
Baumis <baumgartneraxel@gmail.com>
2020-07-28 17:01:03 +0300

    searchbar rework

--- client/src/views/Search/components/SearchBar/SearchBar.js
+++ client/src/views/Search/components/SearchBar/SearchBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import './SearchBar.css'
-import { FaSearch, FaAngleDown } from 'react-icons/fa'
+import { FaSearch, FaAngleDown, FaChevronUp, FaAngleUp, FaFilter } from 'react-icons/fa'
 import Dropdown from '../../../../commonComponents/Dropdown/Dropdown'
 import FilterInput from './FilterInput/FilterInput'
 
@@ -14,7 +14,8 @@ class SearchBar extends Component {
             searchType: 'events',
             date: null,
             location: '',
-            cathegory: 'all'
+            cathegory: 'all',
+            displayFilters: false
         }
     }
 
@@ -30,66 +31,71 @@ class SearchBar extends Component {
         this.setState({ searchType: value })
     }
 
+    toggleFilters = () => {
+        this.setState({ displayFilters: !this.state.displayFilters })
+    }
+
     search = () => {
         if (this.state.inputValue.length > 0) {
             this.props.search(this.state.inputValue)
         }
     }
 
+    tabClassByState = (tab) => {
+        return this.state.searchType === tab ?
+            'searchbar-tab searchbar-selected-tab'
+            : 'searchbar-tab'
+    }
+
     render() {
         return (
             <div className="searchbar">
-                <div className="searchbar-container">
+                <div className="searchbar-content">
                     <div className="serachbar-title">
                         Search events and profiles.
                     </div>
-                    <div className="search-input-row">
-                        <div className="search-input">
-                            <div className="search-serachbar-icon"><FaSearch /></div>
-                            <input
-                                value={this.state.inputValue}
-                                onChange={(event) => this.changeInputValue(event.target.value)}
-                                placeholder={"Name"}
-                            />
+                    <div className="searchbar-container">
+                        <div className="searchbar-tabs">
+                            <div className={`${this.tabClassByState('events')} searchbar-tab-left`} onClick={() => this.changeType('events')}>
+                                Events
                         </div>
-                        <Dropdown
-                            value={this.state.searchType}
-                            items={['events', 'profiles']}
-                            setValue={this.changeType}
-                        />
-                    </div>
-                    {this.state.searchType === 'events' &&
-                        <div className="searchbar-filter-row">
-                            <FilterInput
-                                label={'Date'}
-                                type={'date'}
-                                value={this.state.date}
-                                onChange={(event) => this.setState({ date: event.target.value })}
-                            />
-                            <FilterInput
-                                label={'Location'}
-                                type={'text'}
-                                value={this.state.location}
-                                onChange={(event) => this.setState({ location: event.target.value })}
-                                placeholder={'city, address'}
-                            />
-                            <div className="searchbar-cathegory-select">
-                                <div className="searchbar-cathegory-label">
-                                    Cathegory
-                                </div>
-                                <Dropdown
-                                    value={this.state.cathegory}
-                                    items={['studies', 'all']}
-                                    setValue={(value) => this.setState({ cathegory: value })}
-                                    buttonStyle={{ height: '25px' }}
-                                />
+                            <div className={`${this.tabClassByState('profiles')} searchbar-tab-right`} onClick={() => this.changeType('profiles')}>
+                                Profiles
                             </div>
                         </div>
-                    }
-                    <div className="searchbar-button-row">
-                        <div className="searchbar-submit-button" onClick={this.search}>
-                            Search
+                        <div className="search-input-row">
+                            <div className="search-input">
+                                <div className="search-serachbar-icon"><FaSearch /></div>
+                                <input
+                                    value={this.state.inputValue}
+                                    onChange={(event) => this.changeInputValue(event.target.value)}
+                                    placeholder={`search ${this.state.searchType}`}
+                                />
+                            </div>
+                            {this.state.searchType === 'events' &&
+                                <div className={`searchbar-filter-button ${this.state.displayFilters ? 'filter-active': ''}`} onClick={this.toggleFilters}>
+                                    <div className="filter-text"> filters </div>
+                                    <FaFilter style={{color: this.state.displayFilters ? '#19a45e': ''}}/>
+                                </div>
+                            }
                         </div>
+                        {this.state.searchType === 'events' && this.state.displayFilters &&
+                            <div className="searchbar-filter-row">
+                                <FilterInput
+                                    label={'Date'}
+                                    type={'date'}
+                                    value={this.state.date}
+                                    onChange={(event) => this.setState({ date: event.target.value })}
+                                />
+                                <FilterInput
+                                    label={'Location'}
+                                    type={'text'}
+                                    value={this.state.location}
+                                    onChange={(event) => this.setState({ location: event.target.value })}
+                                    placeholder={'city, address'}
+                                />
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
